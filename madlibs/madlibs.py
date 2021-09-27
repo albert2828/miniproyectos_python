f2d64c2b9f90aeeb630fbe984c89b66634713cb0
Alberto Macias <alberto.macbar9512@gmail.com>
2021-09-27 18:46:54 -0500

    juego historias locas creado

---
+++ madlibs/madlibs.py
@@ -0,0 +1,18 @@
+
+def main():
+    print(40*"=")
+    print(" ¡Bienvenido al juego! \nIntroduce con el teclado lo siguiente")
+    print(40*"=")
+
+    verbo1 = input("Un verbo (por ejemplo 'escribir'): ")
+    verbo2 = input("Un verbo (por ejemplo 'jugar'): ")
+    adjetivo = input("Un sustantivo (por ejemplo 'divertido'): ")
+
+    madlib = f"Hoy quería {verbo1} una historia, pero en lugar de eso, "
+    madlib += f"me puse a {verbo2}. Al final, fue muy {adjetivo}."
+
+    print(madlib)
+    print("¡Gracias por jugar! Vuelve pronto.")
+
+if __name__=='__main__':
+    main()
